fa93adbd65c2a0b764fe970c909b4f81541b4cf7
Kerry Felton <kfelton@ycp.edu>
2012-09-30 16:47:14 -0400

    script

--- inventoryUpdated.sql
+++ inventoryUpdated.sql
@@ -1,1 +1,26 @@
-product_idSELECT * FROM inventory_Updated.SHIRTs;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `inventory_Updated` ;
+USE `inventory_Updated` ;
+
+-- -----------------------------------------------------
+-- Table `inventory_Updated`.`SHIRTs`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `inventory_Updated`.`SHIRTs` (
+  `product_id` INT NOT NULL AUTO_INCREMENT ,
+  `category` VARCHAR(45) NOT NULL ,
+  `style` VARCHAR(45) NOT NULL ,
+  `gender` VARCHAR(45) NOT NULL ,
+  `size` VARCHAR(45) NOT NULL ,
+  `picture` VARCHAR(45) NOT NULL ,
+  `NuminInventory` INT NOT NULL ,
+  PRIMARY KEY (`product_id`, `category`, `style`, `gender`, `size`, `picture`, `NuminInventory`) )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
