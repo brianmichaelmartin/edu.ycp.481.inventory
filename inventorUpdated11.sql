ff8a857c881c4d3d87e4e60a860d56e7e9bbe959
Kerry Felton <kfelton@ycp.edu>
2012-10-07 16:54:58 -0400

    added _ids

--- inventorUpdated11.sql
+++ inventorUpdated11.sql
@@ -44,46 +44,16 @@ CREATE  TABLE IF NOT EXISTS `inventory_Updated`.`GENDER` (
   PRIMARY KEY (`gender_id`, `gender_type`) )
 ENGINE = InnoDB;
 
-INSERT INTO SIZE Values(1, 'S');
-INSERT into SIZE Values (2, 'M');
-INSERT into SIZE values (3, 'L');
-INSERT into SIze values (4, 'XL');
-INSERT into SIze values (5, '2XL');
-INSERT into SIze values (6, '3XL');
-INSERT into SIze values (7, '4XL');
-
-INSERT into GENDER values (1, 'Men');
-INSERT into gender values (2, 'Women');
-Insert into gender values (3, 'Kid');
-
-INSERT into category values (1, 'Retro Button Up Shirts');
-Insert into category values (2, 'Polo Sports Shirts');
-Insert into category values (3, 'T-Shirts');
-Insert into category values (4, 'Polo Bowling Shirts');
-
-Insert into style values (1, 'LASER : Dry-Excel Elite');
-Insert into style values (2, 'Big Dom : 50s Bowling Shirt');
-Insert into style values (3, 'Player : Bowling Lounge Shirt');
-Insert into style values (4, 'Ladies Betty B Button Down Bowling Shirt');
-Insert into style values (5, 'Lady Miami : Womens Bowling Camp Shirt');
-Insert into style values (6, 'Ladies Classic Easy to Look Great Button Down');
-Insert into style values (7, 'Swing kids Retro Shirt');
-Insert into style values (8, 'Kids Rock n Roll 50s Shirt');
-Insert into style values (9, 'Kids Classic Retro 50s');
-
-
-
-
 
 -- -----------------------------------------------------
 -- Table `inventory_Updated`.`SHIRTs`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `inventory_Updated`.`SHIRTs` (
   `product_id` INT NOT NULL AUTO_INCREMENT ,
-  `category` INT NOT NULL ,
-  `style` INT NOT NULL ,
-  `gender` INT NOT NULL ,
-  `size` INT NOT NULL ,
+  `category_id` INT NOT NULL ,
+  `style_id` INT NOT NULL ,
+  `gender_id` INT NOT NULL ,
+  `size_id` INT NOT NULL ,
   `picture` VARCHAR(45) NOT NULL ,
   `NuminInventory` INT NOT NULL ,
   `disabled` TINYINT(1) NOT NULL ,
@@ -99,7 +69,7 @@ CREATE  TABLE IF NOT EXISTS `inventory_Updated`.`SHIRTs` (
   `SIZe_size_type` VARCHAR(45) NOT NULL ,
   `GENDER_gender_id` INT NOT NULL ,
   `GENDER_gender_type` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`product_id`, `category`, `style`, `gender`, `size`, `picture`, `NuminInventory`, `disabled`, `inStock`, `barCode`, `dateAdded`, `lastModified`) ,
+  PRIMARY KEY (`product_id`, `category_id`, `style_id`, `gender_id`, `size_id`, `picture`, `NuminInventory`, `disabled`, `inStock`, `barCode`, `dateAdded`, `lastModified`) ,
   INDEX `fk_SHIRTs_CATEGORY_idx` (`CATEGORY_category_id` ASC, `CATEGORY_category_type` ASC) ,
   INDEX `fk_SHIRTs_STYLE1_idx` (`STYLE_style_id` ASC, `STYLE_style_type` ASC) ,
   INDEX `fk_SHIRTs_SIZe1_idx` (`SIZe_size_id` ASC, `SIZe_size_type` ASC) ,
