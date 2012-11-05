08ddb823531bc1e40b8ffb686df4ff66679e9dc1
brianmichaelmartin <brian.michael.martin@gmail.com>
2012-11-04 20:21:26 -0500

    forgot to save something

--- edu.ycp.cs481.inventory/src/edu/ycp/cs481/inventory/Delete.java
+++ edu.ycp.cs481.inventory/src/edu/ycp/cs481/inventory/Delete.java
@@ -2,6 +2,7 @@ package edu.ycp.cs481.inventory;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Delete {
@@ -12,6 +13,18 @@ public class Delete {
 		
 		String query = "DELETE FROM inventory WHERE " + field + " = " + val;
 		
+		try {
+			stmt = c.createStatement();
+			rs = stmt.executeQuery(query);
+			
+			
+		} catch (SQLException e) {
+
+			//JDBCTutorialUtilities.printSQLException(e);
+
+			System.out.println("there was an issue deleting");
+			return false;
+		}
 		return true;
 	}
 }
